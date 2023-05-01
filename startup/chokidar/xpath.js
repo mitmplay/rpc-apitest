c2a4eb991aabae73ea0580888f98b38b00d87c37
Widi Harsojo <wharsojo@gmail.com>
2023-05-02 01:00:40 +0800

    Fix xpath default

--- startup/chokidar/xpath.js
+++ startup/chokidar/xpath.js
@@ -1,13 +1,15 @@
 const rg1 = /\\/g
 
 function xpath(path) {
-  const {_obj_: {argv}} = rpc()
+  const {_obj_: {HOME, argv}} = rpc()
   path = path.replace(rg1,'/')
   if (path.includes(global.__app)) {
     path = path.replace(`${__app}/RPC/`, '')
   } else if (argv.rpcpath) {
     const p = argv.rpcpath.replace(/[/ ]+$/g, '')
     path = path.replace(`${p}/`, '')
+  } else {
+    path = path.replace(`${HOME}/user-rpc/`, '')
   }
   const [app, typ, ...rest] = path.split('/')
   const name = rest.join('/')
