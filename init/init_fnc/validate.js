38d096fe1f1debc23518c80f4fcec6dccdd40c4e
Widi Harsojo <wharsojo@gmail.com>
2023-06-04 21:16:16 +0800

    add validation reqs & resp 03

--- init/init_fnc/validate.js
+++ init/init_fnc/validate.js
@@ -1,3 +1,33 @@
+function checkMissingFields(payload, schemas, path, result) {
+  for (const id in schemas) {
+    const schemas2 = schemas[id]
+    const payload2 = payload[id]
+    if (path!=='.' && schemas2 && payload2===undefined) {
+      result.missing_fields.push(`${path.slice(1)}${id}`)
+      result.invalid = result.errid
+    }
+    if (schemas2!==null && typeof schemas2==='object') { // recursive 
+      checkMissingFields(payload2, schemas2, `${path}${id}.`, result)
+    }
+  }
+  return result
+}
+
+function checkUnknownFields(payload, schemas, path, result) {
+  for (const id in payload) {
+    const schemas2 = schemas[id]
+    const payload2 = payload[id]
+    if (path!=='.' && schemas2===undefined) {
+      result.unknown_fields.push(`${path.slice(1)}${id}`)
+      result.invalid = result.errid
+    }
+    if (schemas2!==null && typeof schemas2==='object') { // recursive 
+      checkUnknownFields(payload2, schemas2, `${path}${id}.`, result)
+    }
+  }
+  return result
+}
+
 function validate({reqs, rspcode, resp_hdr, resp_body, validate}) {
   const {request='', response=''} = validate
   let reqs_schema = request[reqs.method] || request
@@ -15,40 +45,27 @@ function validate({reqs, rspcode, resp_hdr, resp_body, validate}) {
     return {invalid: 0, err: []}
   }
 
-  function checkMissingFields(payload, schemas, path, result) {
-    for (const id in schemas) {
-      const schemas2 = schemas[id]
-      const payload2 = payload[id]
-      if (schemas2 && payload2===undefined) {
-        result.missing_fields.push(`${path.slice(1)}${id}`)
-        result.invalid = result.errid
-      }
-      if (schemas2!==null && typeof schemas2==='object') { // recursive 
-        checkMissingFields(payload2, schemas2, `${path}${id}.`, result)
-      }
-    }
-    return result
-  }
-
   const json = {invalid: 0}
   if (reqs_schema) {
     const jreqs = {invalid: 0, missing_fields: [], unknown_fields: [], errid: 1}
-    const _reqs = checkMissingFields(reqs, reqs_schema, '.', jreqs)
-    if (_reqs.invalid) {
-      json.invalid+= _reqs.invalid
-      json.reqs    = _reqs
-      delete _reqs.errid  
+    checkMissingFields(reqs, reqs_schema, '.', jreqs)
+    checkUnknownFields(reqs, reqs_schema, '.', jreqs)
+    if (jreqs.invalid) {
+      json.invalid+= jreqs.invalid
+      json.reqs    = jreqs
+      delete jreqs.errid  
     }
   }
 
   if (resp_schema) {
     const resp = {headers: resp_hdr, body: resp_body}
     const jresp = {invalid: 0, missing_fields: [], unknown_fields: [], errid: 2}
-    const _resp = checkMissingFields(resp, resp_schema, '.', jresp)
-    if (_resp.invalid) {
-      json.invalid+= _resp.invalid
-      json.resp    = _resp
-      delete _resp.errid  
+    checkMissingFields(resp, resp_schema, '.', jresp)
+    checkUnknownFields(resp, resp_schema, '.', jresp)
+    if (jresp.invalid) {
+      json.invalid+= jresp.invalid
+      json.resp    = jresp
+      delete jresp.errid  
     }
   }
 
